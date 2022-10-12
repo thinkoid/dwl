4c1104a0a6dadfa3afe1baeac05bdce117149630
Liviu Nicoara <lnicoara at thinkoid dot org>
2022-10-12 14:32:21 -0400

    chore: customized Makefiles

--- config.mk
+++ config.mk
@@ -1,14 +1,11 @@
-_VERSION = 0.3.1-dev
-VERSION  = `git describe --long --tags --dirty 2>/dev/null || echo $(_VERSION)`
+_VERSION := 0.3.1-dev
+VERSION := $(shell git describe --long --tags --dirty 2>/dev/null || echo $(_VERSION))
 
 PKG_CONFIG = pkg-config
 
-# paths
 PREFIX = /usr/local
 MANDIR = $(PREFIX)/share/man
 
-# XWAYLAND =
-# XLIBS =
-# Uncomment to build XWayland support
+# XWayland support
 XWAYLAND = -DXWAYLAND
 XLIBS = xcb
