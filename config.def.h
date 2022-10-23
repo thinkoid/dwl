b029641cc7d147cd73b5caf7ddc0d904112c1603
Liviu Nicoara <lnicoara at thinkoid dot org>
2022-10-23 10:48:33 -0400

    feat: more customizations

--- config.def.h
+++ config.def.h
@@ -84,7 +84,7 @@ LIBINPUT_CONFIG_ACCEL_PROFILE_FLAT
 LIBINPUT_CONFIG_ACCEL_PROFILE_ADAPTIVE
 */
 static const enum libinput_config_accel_profile accel_profile = LIBINPUT_CONFIG_ACCEL_PROFILE_ADAPTIVE;
-static const double accel_speed = 0.0;
+static const double accel_speed = 0.95;
 
 /* If you want to use the windows key change this to WLR_MODIFIER_LOGO */
 #define MODKEY WLR_MODIFIER_ALT
@@ -98,8 +98,8 @@ static const double accel_speed = 0.0;
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *termcmd[] = { "foot", NULL };
-static const char *menucmd[] = { "bemenu-run", "--fn", "JetBrains Mono Thin 18", "-n", NULL };
+static const char *termcmd[] = { "foot", 0 };
+static const char *menucmd[] = { "bemenu-run", 0 };
 
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
